7055eb83047844e7a6a9e3dd905987742f0f4799
jayson <jayson@3hgoods.com>
2022-08-15 01:37:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-sudo apt-get install -y docker.io
-
-sudo apt-get install -y curl
-
-sudo apt-get install -y npm
-
-sudo curl -L https://github.com/docker/compose/releases/download/1.3.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 
 # ref https://www.theserverside.com/blog/Coffee-Talk-Java-News-Stories-and-Opinions/How-to-install-Docker-and-docker-compose-on-Ubuntu
 
-sudo chmod +x /usr/local/bin/docker-compose
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
+
+### Docker and docker compose prerequisites
+sudo apt-get install curl -y
+sudo apt-get install gnupg -y
+sudo apt-get install ca-certificates -y
+sudo apt-get install lsb-release -y
+
+### Download the docker gpg file to Ubuntu
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+### Add Docker and docker compose support to the Ubuntu's packages list
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+### Verify the Docker and docker compose install on Ubuntu
+sudo docker run hello-world
+
 
 git clone https://github.com/IoTKETI/Mobius.git
 
@@ -21,9 +32,7 @@ sudo npm install
 cd ..
 
 mkdir config
-mkdir data
-mkdir log
-cp mosquitto.conf ./config
+cp ./mosquitto.conf ./config
 
 sudo docker-compose up --no-start
 
